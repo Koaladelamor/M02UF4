0a75227469f48b1769915dc4242e3aa1f9c64b0a
enti <enti@enti.enti>
2022-02-04 11:17:06 +0100

    Pequeños cambios

--- index.js
+++ index.js
@@ -2,6 +2,29 @@
 
 let http = require("http");
 
+let mongo_client = require("mongodb").MongoClient;
+
+let url = "mongodb://localhost/";
+
+console.log("Iniciando script mongo-http");
+
+mongo_client.connect(url, function(error, conn){
+	console.log("Dentro de MongoDB");
+	console.log(error);
+
+	if (error) {
+		console.log("ERROR!");
+		return;
+	}
+
+	let db = conn.db("tffhd");
+
+	let characters = db.collection("characters").find();
+	console.log(characters);
+
+});
+
+/*
 http.createServer(function(request, response){
 	response.writeHead(200);
 
@@ -10,6 +33,6 @@ http.createServer(function(request, response){
 	response.end(saludo);
 
 }).listen(1095);
-
+*/
 
 
